5c2df7498926dab20c130b26904d12957505588a
dhommen <dh@orbiter.de>
2023-07-10 13:38:28 +0000

    add debugger

---
+++ scripts/start_debug.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+#
+# Shell Script to start 
+# Author: truzzt GmbH
+# Copyright 2023
+
+source scripts/load_environment.sh
+
+# Extract SKI and AKI from client certificate
+BROKER_SKI="$(openssl x509 -in "data/cert/broker.dev.truzzt.com.crt" -noout -text | awk '/Subject Key Identifier/ {getline; print}' | tr -d ' ')"
+BROKER_AKI="$(openssl x509 -in "data/cert/broker.dev.truzzt.com.crt" -noout -text | awk '/Authority Key Identifier/ {getline; print}' | tr -d ' ')"
+
+CONNECTOR_SKI="$(openssl x509 -in "data/cert/connector.dev.truzzt.com.crt" -noout -text | awk '/Subject Key Identifier/ {getline; print}' | tr -d ' ')"
+CONNECTOR_AKI="$(openssl x509 -in "data/cert/connector.dev.truzzt.com.crt" -noout -text | awk '/Authority Key Identifier/ {getline; print}' | tr -d ' ')"
+
+export TRUZZTPORT_BROKER_SKI_AKI=$BROKER_SKI:$BROKER_AKI
+export TRUZZTPORT_CONNECTOR_SKI_AKI=$CONNECTOR_SKI:$CONNECTOR_AKI
+
+docker compose -f docker/docker-compose.yml -f docker/docker-compose.traefik.yml -f docker/docker-compose.debug.yml up -d
