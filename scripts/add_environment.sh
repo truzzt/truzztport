3fa37bf67b0142b66af10ab59213452bf901b94f
dhommen <dh@orbiter.de>
2023-07-27 14:40:14 +0200

    feat: add option to set TRUZZTPORT_ENV_SLUG as ARG

--- scripts/add_environment.sh
+++ scripts/add_environment.sh
@@ -4,6 +4,24 @@
 # Author: truzzt GmbH
 # Copyright 2023
 
+if [ $# -eq 1 ]; then
+  echo "Got Argument $1"
+
+  # Check if the .env file exists
+  ENV_FILE=".env"
+
+  # Check the value of the OSTYPE variable
+  if [[ "$OSTYPE" == "darwin"* ]]; then
+      sed -i "" "s/^TRUZZTPORT_ENV_SLUG=.*/TRUZZTPORT_ENV_SLUG=$1/" $ENV_FILE
+  elif [[ "$OSTYPE" == "linux-gnu" ]]; then
+      sed -i "s/^TRUZZTPORT_ENV_SLUG=.*/TRUZZTPORT_ENV_SLUG=$1/" $ENV_FILE
+  else
+    echo "Unsupported operating system"
+  fi
+
+
+fi
+
 source scripts/load_environment.sh
 source scripts/setups/setup_directories.sh
 source scripts/setups/setup_subca.sh
